365173bbf18a4906c867a8697b366d4f9ef6b456
hangltt3 <hangltt3@vng.com.vn>
2021-05-23 18:00:55 +0700

    update paragraph seg for english

--- english/main.py
+++ english/main.py
@@ -4,10 +4,9 @@ from preprocessing import PreProcessing
 if __name__ == "__main__":
     sample_text = """
         ADD MORE: 1234456 <head> Thuy Hang </head>
-        These steps should give you a good idea about how to build your clean-up and pre-processing strategies. 
-        After these pre-processing steps, the corpus of text is ready for NLP algorithms such as Word2Vec, GloVe etc. These pre-processing steps will definitely improve the model’s accuracy.
+                 These steps should give you a good idea about how to build your clean-up and pre-processing strategies. After these pre-processing steps, the corpus of text is ready for NLP algorithms such as Word2Vec, GloVe etc. These pre-processing steps will definitely improve the model’s accuracy.
 
-        Hope you liked this article, and if so, then please say so in comments. If you would like to share any suggestion on any approach, then feel free to say so in comments and I will revert right away.
+  Hope you liked this article, and if so, then please say so in comments. If you would like to share any suggestion on any approach, then feel free to say so in comments and I will revert right away.
     """
 
     process = PreProcessing(sample_text)
@@ -15,3 +14,6 @@ if __name__ == "__main__":
     cleantext = process.show_text()
     print("==========TEXT AFTER CLEAN STEP============")
     print(cleantext)
+    paragraphs = process.step_paragraph_segmentation()
+    print("==========TEXT AFTER PARAGRAPH SEGMENTATION==========")
+    print(paragraphs)
--- english/preprocessing.py
+++ english/preprocessing.py
@@ -9,15 +9,29 @@ parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
 sys.path.insert(0, parent_dir)
 
 from utils.clean import CleanData
+from utils.paragraph_segmentation import ParagraphSegmentation
 
-class PreProcessing(CleanData):
+class PreProcessing(CleanData, ParagraphSegmentation):
     def __init__(self, text):
         self.text = text
+        ParagraphSegmentation.__init__(self, self.text, 'english')
 
     def step_clean(self):
         textRemoveHTMLTag = self.remove_html_tags(self.text)
         textRemoveSpecialCharacter = self.remove_special_characters_eng(textRemoveHTMLTag)
         self.text = textRemoveSpecialCharacter
+
+    def step_paragraph_segmentation(self):
+        self.get_paragraphs()
+        paragraphs = self.show_paragraphs()
+        new_paragraphs = []
+        for paragraph in paragraphs:
+            paragraph_remove_count_number = self.remove_count_number(paragraph)
+            paragraph_remove_number = self.remove_numbers_eng(paragraph_remove_count_number)
+            paragraph_remove_extra_whitespace_tabs = self.remove_extra_whitespace_tabs(paragraph_remove_number)
+            paragraph_to_lowercase = self.to_lowercase(paragraph_remove_extra_whitespace_tabs)
+            new_paragraphs.append(paragraph_to_lowercase)
+        return new_paragraphs
 
     def show_text(self):
         return self.text
