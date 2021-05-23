f1394c945c09d97dd5989aad8f79ae1ff369a3b8
hangltt3 <hangltt3@vng.com.vn>
2021-05-23 15:00:06 +0700

    add clean data for vietnamese

---
+++ vietnamese/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from preprocessing import PreProcessing
+
+if __name__ == "__main__":
+    sample_text = """
+                    <html> <body>
+                    BIẾT ƠN 123456
+                    Cảm giác luôn là sự biết ơn, vì khi đi vào Sài Gòn đông đúc này, mới nhận ra rằng quanh mình luôn có những người yêu thương, quan tâm, giúp đỡ và luôn dõi theo. 
+                    Không được ở chung với gia đình mới thấy rằng gia đình là nơi yêu thương vô điều kiện. Mỗi lần FaceTime là Ba lúc nào cũng nói “ Răng bữa ni mập rứa con, mập rứa ko ai ưa mô, coi giảm cân đi nghe” 
+                    Trước đây rất ít khi thấy Ba nói ra những câu như vậy, chắc do con gái đi xa nên Ba lo hihi. 
+                    Còn bà chị già khó tính của tui là lúc mô cũng nhắc rửa tay, uống nước cam, dặn lo phòng dịch... và thế là bà chị ko tin tưởng tui gửi vào Sài gòn cho con bé HECTOR SÂM để tăng sức đề kháng mùa dịch này
+                    </body></html>
+                """
+    
+    process = PreProcessing(sample_text)
+    process.stepClean()
+    cleantext = process.showText()
+    print("==========TEXT AFTER CLEAN STEP============")
+    print(cleantext)
---
+++ vietnamese/preprocessing.py
@@ -0,0 +1,28 @@
+from inspect import getsourcefile
+import os.path
+import sys
+
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+
+from utils.clean import CleanData
+
+
+class PreProcessing(CleanData):
+    def __init__(self, text):
+        self.text = text
+
+    def stepClean(self):
+        textRemoveHTMLTag = self.remove_html_tags(self.text)
+        textToLowercase = self.to_lowercase(textRemoveHTMLTag)
+        textRemoveSpecialCharacter = self.remove_special_characters_vn(textToLowercase)
+        textRemoveNumber = self.remove_numbers_vn(textRemoveSpecialCharacter)
+        self.text = textRemoveNumber
+
+    def showText(self):
+        return self.text
+
+    
