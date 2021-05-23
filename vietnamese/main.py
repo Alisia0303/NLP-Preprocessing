afad6a01a81e7d1ea90e47acfae8d5fec0454452
hangltt3 <hangltt3@vng.com.vn>
2021-05-23 21:53:27 +0700

    add sentence_segmentation for vietnamese

--- vietnamese/main.py
+++ vietnamese/main.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
  BIẾT ƠN 123456.
  Cảm giác luôn là sự biết ơn, vì khi đi vào Sài Gòn đông đúc này, 
 mới nhận ra rằng quanh mình luôn có những người yêu thương, quan tâm, giúp đỡ và luôn dõi theo.
- Không được ở chung với gia đình mới thấy rằng gia đình là nơi yêu thương vô điều
-kiện. Mỗi lần FaceTime là Ba lúc nào cũng nói “ Răng bữa ni mập rứa con, mập rứa ko ai ưa mô, coi giảm cân đi nghe”.
+ Không được ở chung với gia đình mới thấy rằng gia đình là nơi yêu thương vô điều 
+kiện. Mỗi lần FaceTime là Ba lúc nào cũng nói “ Răng bữa ni mập rứa con, mập rứa ko ai ưa mô, https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles coi giảm cân đi nghe”.
     
     Trước đây rất ít khi thấy Ba nói ra những câu như vậy, chắc do con gái đi xa nên Ba lo hihi. 
 
@@ -30,4 +30,6 @@ kiện. Mỗi lần FaceTime là Ba lúc nào cũng nói “ Răng bữa ni mậ
     paragraphs = process.step_paragraph_segmentation()
     print("==========TEXT AFTER PARAGRAPH SEGMENTATION==========")
     print(paragraphs)
-
+    sentences = process.step_sentence_segmentation(paragraphs)
+    print("==========TEXT AFTER SENTENCE SEGMENTATION===========")
+    print(sentences)
